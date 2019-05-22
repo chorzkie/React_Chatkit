b52a13f1b2c8d08776e32049d54222d13f8ca2d8
Ari <chorzkie@gmail.com>
2019-05-22 13:50:33 +0300

    Updates

---
+++ precache-manifest.46febff6813e24bbf6c7e31e89c112c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5087102d405e3e16bf78e8c23d4e7d37",
+    "url": "/React_Chatkit/index.html"
+  },
+  {
+    "revision": "7c14ac96dbfcca13fcdd",
+    "url": "/React_Chatkit/static/css/main.e35367b9.chunk.css"
+  },
+  {
+    "revision": "7b327f232ec2de41d19a",
+    "url": "/React_Chatkit/static/js/2.338adfdb.chunk.js"
+  },
+  {
+    "revision": "7c14ac96dbfcca13fcdd",
+    "url": "/React_Chatkit/static/js/main.259af89f.chunk.js"
+  },
+  {
+    "revision": "22059ea005d399ef1407",
+    "url": "/React_Chatkit/static/js/runtime~main.ff6e1e57.js"
+  }
+]);
